2137545c4fb6a358f400752342c64ff8484e09f9
Konstantinos Grontis <kog@sitecore.net>
2023-06-23 14:25:06 +0300

    #3  Implemented GetMonthlyCosts method and added appropriate tests.

---
+++ subtrack.MAUI/Services/SubscriptionsCalculator.cs
@@ -0,0 +1,13 @@
+﻿using subtrack.DAL.Entities;
+
+namespace subtrack.MAUI.Services;
+
+public static class SubscriptionsCalculator
+{
+    public static decimal GetMonthlyCost(IEnumerable<Subscription> subscriptions)
+    {
+        if (subscriptions is null || !subscriptions.Any()) return 0;
+        var price = subscriptions.Sum(x => x.Cost);
+        return price;
+    }
+}
---
+++ subtrack.Tests/SubscriptionCalculatorTests/GetMonthlyCostTests.cs
@@ -0,0 +1,55 @@
+﻿using subtrack.DAL.Entities;
+using subtrack.MAUI.Services;
+
+namespace subtrack.Tests.SubscriptionCalculatorTests;
+
+public class GetMonthlyCostTests
+{
+    [Fact]
+    public void GetMonthlyCost_EmptySubscriptions_ReturnsZero()
+    {
+        var subscriptions = new List<Subscription>();
+
+        var result = SubscriptionsCalculator.GetMonthlyCost(subscriptions);
+
+        Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void GetMonthlyCost_NullSubscriptions_ReturnsZero()
+    {
+        List<Subscription>? subscriptions = null;
+
+        var result = SubscriptionsCalculator.GetMonthlyCost(subscriptions);
+
+        Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void GetMonthlyCost_OneSubscription_ReturnsCorrectMonthlyCost()
+    {
+        var subscriptions = new List<Subscription>
+        {
+            new Subscription { Cost = 10.5m }
+        };
+
+        var result = SubscriptionsCalculator.GetMonthlyCost(subscriptions);
+
+        Assert.Equal(10.5m, result);
+    }
+
+    [Fact]
+    public void GetMonthlyCost_MultipleSubscriptions_ReturnsCorrectMonthlyCost()
+    {
+        var subscriptions = new List<Subscription>
+        {
+            new Subscription { Cost = 10.5m },
+            new Subscription { Cost = 5.25m },
+            new Subscription { Cost = 7.75m }
+        };
+
+        var result = SubscriptionsCalculator.GetMonthlyCost(subscriptions);
+
+        Assert.Equal(23.5m, result);
+    }
+}
