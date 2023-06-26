7deb5a7347ba5567671ec26909c8f3797d3ca91c
Spark <piotr.rac@gmail.com>
2023-06-26 19:27:45 +0200

    Modified method to return price for a relevant month

--- subtrack.MAUI/Services/SubscriptionsCalculator.cs
+++ subtrack.MAUI/Services/SubscriptionsCalculator.cs
@@ -7,7 +7,16 @@ public static class SubscriptionsCalculator
     public static decimal GetMonthlyCost(IEnumerable<Subscription> subscriptions)
     {
         if (subscriptions is null || !subscriptions.Any()) return 0;
-        var price = subscriptions.Sum(x => x.Cost);
-        return price;
+        var subscriptionsByMonth = GetSubscriptionListByMonth(subscriptions, DateTime.Now);
+        var monthlyPrice = subscriptionsByMonth.Sum(x => x.Cost);
+        return monthlyPrice;
+    }
+
+    public static List<Subscription> GetSubscriptionListByMonth(IEnumerable<Subscription> subscriptions, DateTime localDate)
+    {
+        var subscriptionsByMonth = subscriptions
+                    .Where(s => s.LastPayment.Month != localDate.Month)
+                    .ToList();
+        return subscriptionsByMonth;
     }
 }
