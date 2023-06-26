d955924a41d9418033ef0dd32d75d7597b24e97c
Spark <piotr.rac@gmail.com>
2023-06-26 19:30:17 +0200

    Initial view model

---
+++ subtrack.MAUI/Data/SubscriptionResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace subtrack.MAUI.Data
+{
+    public class SubscriptionResponse
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = null!;
+        public string? Description { get; set; }
+        public bool IsAutoPaid { get; set; }
+        public decimal Cost { get; set; }
+        public DateTime LastPayment { get; set; }
+    }
+}
---
+++ subtrack.MAUI/Response/SubscriptionsMonthResponse.cs
@@ -0,0 +1,48 @@
+﻿using subtrack.MAUI.Services;
+using subtrack.DAL.Entities;
+using subtrack.MAUI.Data;
+
+namespace subtrack.MAUI.Response
+{
+    public class SubscriptionsMonthResponse
+    {
+        public DateTime CurrentDate { get; set; }
+        public IEnumerable<Subscription> Subscriptions { get; }
+        public IEnumerable<SubscriptionResponse> SubscriptionResponse { get; }
+
+        public SubscriptionsMonthResponse(DateTime dateTime)
+        {
+            CurrentDate = dateTime;
+        }
+
+        public IEnumerable<SubscriptionResponse> GetSubscriptionCostByMonth()
+        {
+            var subscriptionByMonth = SubscriptionsCalculator.GetSubscriptionListByMonth(Subscriptions, CurrentDate);
+            var subscriptionViewModel = Map(subscriptionByMonth);
+
+            return subscriptionViewModel;
+        }
+
+        public IEnumerable<SubscriptionResponse> Map(IEnumerable<Subscription> Subscriptions)
+        {
+            var subscriptionResponse = new List<SubscriptionResponse>();
+
+            foreach (var subscription in Subscriptions)
+            {
+                var SubscriptionResponse = new SubscriptionResponse 
+                {
+                    Id = subscription.Id,
+                    Name = subscription.Name,
+                    Description = subscription?.Description,
+                    IsAutoPaid = subscription.IsAutoPaid,
+                    Cost = subscription.Cost,
+                    LastPayment = subscription.LastPayment
+                };
+
+                subscriptionResponse.Add(SubscriptionResponse);
+            }
+
+            return subscriptionResponse;
+        }
+    }
+}
