70e59aac215e0c50c79fb999a310e1085a617a01
Spark <piotr.rac@gmail.com>
2023-06-27 17:04:33 +0200

    Removed unnecessary ctor, add COST prop and moved mapper to seperate class

--- subtrack.MAUI/Response/SubscriptionsMonthResponse.cs
+++ subtrack.MAUI/Response/SubscriptionsMonthResponse.cs
@@ -7,42 +7,12 @@ namespace subtrack.MAUI.Response
     public class SubscriptionsMonthResponse
     {
         public DateTime CurrentDate { get; set; }
-        public IEnumerable<Subscription> Subscriptions { get; }
+        public decimal Cost { get; set; }
         public IEnumerable<SubscriptionResponse> SubscriptionResponse { get; }
 
         public SubscriptionsMonthResponse(DateTime dateTime)
         {
             CurrentDate = dateTime;
-        }
-
-        public IEnumerable<SubscriptionResponse> GetSubscriptionCostByMonth()
-        {
-            var subscriptionByMonth = SubscriptionsCalculator.GetSubscriptionListByMonth(Subscriptions, CurrentDate);
-            var subscriptionViewModel = Map(subscriptionByMonth);
-
-            return subscriptionViewModel;
-        }
-
-        public IEnumerable<SubscriptionResponse> Map(IEnumerable<Subscription> Subscriptions)
-        {
-            var subscriptionResponse = new List<SubscriptionResponse>();
-
-            foreach (var subscription in Subscriptions)
-            {
-                var SubscriptionResponse = new SubscriptionResponse 
-                {
-                    Id = subscription.Id,
-                    Name = subscription.Name,
-                    Description = subscription?.Description,
-                    IsAutoPaid = subscription.IsAutoPaid,
-                    Cost = subscription.Cost,
-                    LastPayment = subscription.LastPayment
-                };
-
-                subscriptionResponse.Add(SubscriptionResponse);
-            }
-
-            return subscriptionResponse;
         }
     }
 }
