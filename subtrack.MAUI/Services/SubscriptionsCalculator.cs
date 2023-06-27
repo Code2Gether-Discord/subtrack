2646786cb0c96fe3cb0b76627a84e611de0fb01f
Spark <piotr.rac@gmail.com>
2023-06-27 17:05:43 +0200

    Changed GetMonthlyCost to GetTotalPrice and restored logic. Changed return type of GetSubscriptionListByMonth method.

--- subtrack.MAUI/Services/SubscriptionsCalculator.cs
+++ subtrack.MAUI/Services/SubscriptionsCalculator.cs
@@ -4,15 +4,14 @@ namespace subtrack.MAUI.Services;
 
 public static class SubscriptionsCalculator
 {
-    public static decimal GetMonthlyCost(IEnumerable<Subscription> subscriptions)
+    public static decimal GetTotalPrice(IEnumerable<Subscription> subscriptions)
     {
         if (subscriptions is null || !subscriptions.Any()) return 0;
-        var subscriptionsByMonth = GetSubscriptionListByMonth(subscriptions, DateTime.Now);
-        var monthlyPrice = subscriptionsByMonth.Sum(x => x.Cost);
-        return monthlyPrice;
+        var price = subscriptions.Sum(x => x.Cost);
+        return price;
     }
 
-    public static List<Subscription> GetSubscriptionListByMonth(IEnumerable<Subscription> subscriptions, DateTime localDate)
+    public static IEnumerable<Subscription> GetSubscriptionListByMonth(IEnumerable<Subscription> subscriptions, DateTime localDate)
     {
         var subscriptionsByMonth = subscriptions
                     .Where(s => s.LastPayment.Month != localDate.Month)
