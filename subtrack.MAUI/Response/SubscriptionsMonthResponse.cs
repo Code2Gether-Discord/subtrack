3dbd85e21cfefbd4474b0f101ed0614b585bbf3d
Spark <piotr.rac@gmail.com>
2023-06-27 18:19:57 +0200

    Removed ctor

--- subtrack.MAUI/Response/SubscriptionsMonthResponse.cs
+++ subtrack.MAUI/Response/SubscriptionsMonthResponse.cs
@@ -1,18 +1,9 @@
-﻿using subtrack.MAUI.Services;
-using subtrack.DAL.Entities;
-using subtrack.MAUI.Data;
-
-namespace subtrack.MAUI.Response
+﻿namespace subtrack.MAUI.Response
 {
     public class SubscriptionsMonthResponse
     {
         public DateTime CurrentDate { get; set; }
         public decimal Cost { get; set; }
-        public IEnumerable<SubscriptionResponse> SubscriptionResponse { get; }
-
-        public SubscriptionsMonthResponse(DateTime dateTime)
-        {
-            CurrentDate = dateTime;
-        }
+        public IEnumerable<SubscriptionResponse> SubscriptionResponses { get; set; }
     }
 }
