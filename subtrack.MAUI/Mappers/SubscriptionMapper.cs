e2bdb8e0607ad05dd917ffb408be27e35bba8b09
Spark <piotr.rac@gmail.com>
2023-06-27 17:03:23 +0200

    Created mapper class

---
+++ subtrack.MAUI/Mappers/SubscriptionMapper.cs
@@ -0,0 +1,23 @@
+﻿using subtrack.DAL.Entities;
+using subtrack.MAUI.Data;
+
+namespace subtrack.MAUI.Mappers
+{
+    public class SubscriptionMapper
+    {
+        public IEnumerable<SubscriptionResponse> Map(IEnumerable<Subscription> subscriptions)
+        {
+            var subscriptionResponse = subscriptions.Select(subscription => new SubscriptionResponse
+            {
+                Id = subscription.Id,
+                Name = subscription.Name,
+                Description = subscription?.Description,
+                IsAutoPaid = subscription.IsAutoPaid,
+                Cost = subscription.Cost,
+                LastPayment = subscription.LastPayment
+            }).ToList();
+
+            return subscriptionResponse;
+        }
+    }
+}
