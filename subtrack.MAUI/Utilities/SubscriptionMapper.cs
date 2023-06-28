8682c783b12e7b9617211459c0482edfd210ce80
Pituzek <piotr.rac@gmail.com>
2023-06-29 01:24:35 +0200

    Mapper merge

--- subtrack.MAUI/Utilities/SubscriptionMapper.cs
+++ subtrack.MAUI/Utilities/SubscriptionMapper.cs
@@ -6,10 +6,16 @@ namespace subtrack.MAUI.Utilities
 {
     public static class SubscriptionMapper
     {
-        public static IEnumerable<SubscriptionResponse> GetSubscriptionResponses(IEnumerable<Subscription> subscriptions)
+        public static IEnumerable<SubscriptionResponse> ToResponses(this IEnumerable<Subscription> subscriptions)
         {
             return subscriptions.Select(sub => new SubscriptionResponse
             {
+                Id = sub.Id,
+                Name = sub.Name,
+                Description = sub?.Description,
+                IsAutoPaid = sub.IsAutoPaid,
+                Cost = sub.Cost,
+                LastPayment = sub.LastPayment,
                 Subscription = sub,
                 DueDays = SubscriptionsCalculator.GetDueDays(sub)
             });
