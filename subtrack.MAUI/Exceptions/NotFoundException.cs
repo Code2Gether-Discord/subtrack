9c891b036ae70efe0d45baca101e502ddae0088b
Pituzek <piotr.rac@gmail.com>
2023-07-01 23:47:40 +0200

    Exception naming fix

---
+++ subtrack.MAUI/Exceptions/NotFoundException.cs
@@ -0,0 +1,14 @@
+﻿namespace subtrack.MAUI.Exceptions
+{
+    [Serializable]
+    public class NotFoundException : Exception
+    {
+        public NotFoundException() { }
+
+        public NotFoundException(string message)
+            : base(message) { }
+
+        public NotFoundException(string message, Exception inner)
+            : base(message, inner) { }
+    }
+}
