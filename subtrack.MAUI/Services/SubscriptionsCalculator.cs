142f1f4ce165656b49eb37dbe3a2238c050844c4
ChrisK00 <christian.khalil2000@gmail.com>
2023-07-01 23:23:22 +0200

    getyearlycost

--- subtrack.MAUI/Services/SubscriptionsCalculator.cs
+++ subtrack.MAUI/Services/SubscriptionsCalculator.cs
@@ -4,9 +4,13 @@ namespace subtrack.MAUI.Services;
 
 public static class SubscriptionsCalculator
 {
+    private const int _monthsInAYear = 12;
+
     public static decimal GetMonthlyCost(IEnumerable<Subscription> subscriptions)
     {
-        if (subscriptions is null || !subscriptions.Any()) return 0;
+        if (subscriptions is null || !subscriptions.Any())
+            return 0;
+
         var price = subscriptions.Sum(x => x.Cost);
         return price;
     }
@@ -14,12 +18,18 @@ public static class SubscriptionsCalculator
     public static int GetDueDays(Subscription subscription)
     {
         if (subscription is null)
-        {
             throw new ArgumentNullException(nameof(subscription));
-        }
+
         var dueDate = subscription.LastPayment.AddMonths(1).Date;
         var duration = dueDate.Subtract(DateTime.Now.Date);
         return duration.Days;
     }
 
+    public static decimal GetYearlyCost(Subscription subscription)
+    {
+        if (subscription is null)
+            throw new ArgumentNullException(nameof(subscription));
+
+        return subscription.Cost * _monthsInAYear;
+    }
 }
