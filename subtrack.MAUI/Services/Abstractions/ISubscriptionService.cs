924ee9db79b2e8779de661e6ef87bd550b7f52fb
grodis <grodiscostas@hotmail.com>
2023-07-03 13:37:22 +0300

    #31 Implemented CreateSubscriptionAsync method

--- subtrack.MAUI/Services/Abstractions/ISubscriptionService.cs
+++ subtrack.MAUI/Services/Abstractions/ISubscriptionService.cs
@@ -5,5 +5,6 @@ namespace subtrack.MAUI.Services.Abstractions
     internal interface ISubscriptionService
     {
         Task<IEnumerable<Subscription>> GetSubscriptions();
+        Task<Subscription> CreateSubscriptionAsync(Subscription subscriptionToCreate);
     }
 }
