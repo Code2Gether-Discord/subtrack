4d6c55630dfe8056421b2b170fd2811de5c13a10
Pituzek <piotr.rac@gmail.com>
2023-07-03 22:34:21 +0200

    Added service

---
+++ subtrack.Tests/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using subtrack.Tests.DateAndTimeProvider;
+
+namespace subtrack.Tests.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddServices(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddScoped<IDateTimeProvider, DateTimeProvider>();
+
+            return serviceCollection;
+        }
+    }
+}
