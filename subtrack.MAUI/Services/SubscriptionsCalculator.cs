e19171cc5fd0d3187c43811d5ee8768fe518c570
Pituzek <piotr.rac@gmail.com>
2023-07-07 00:16:01 +0200

    Injected subs calc

--- subtrack.MAUI/Services/SubscriptionsCalculator.cs
+++ subtrack.MAUI/Services/SubscriptionsCalculator.cs
@@ -1,9 +1,9 @@
 ﻿using subtrack.DAL.Entities;
-using subtrack.MAUI.DateAndTimeProvider;
+using subtrack.MAUI.Services.Abstractions;
 
 namespace subtrack.MAUI.Services;
 
-public class SubscriptionsCalculator
+public class SubscriptionsCalculator : ISubscriptionsCalculator
 {
     private const int _monthsInAYear = 12;
 
@@ -12,24 +12,6 @@ public class SubscriptionsCalculator
     public SubscriptionsCalculator(IDateTimeProvider dateTimeProvider)
     {
         _dateTimeProvider = dateTimeProvider;
-    }
-
-    public static decimal GetTotalCost(IEnumerable<Subscription> subscriptions)
-    {
-        if (subscriptions is null || !subscriptions.Any())
-            return 0;
-
-        var price = subscriptions.Sum(x => x.Cost);
-        return price;
-    }
-
-    public static IEnumerable<Subscription> GetSubscriptionListByMonth(IEnumerable<Subscription> subscriptions, int month)
-    {
-        var subscriptionsByMonth = subscriptions
-                    .Where(s => s.LastPayment.Month != month)
-                    .ToList();
-
-        return subscriptionsByMonth;
     }
 
     public int GetDueDays(Subscription subscription)
@@ -42,11 +24,29 @@ public class SubscriptionsCalculator
         return duration.Days;
     }
 
-    public static decimal GetYearlyCost(Subscription subscription)
+    public decimal GetTotalCost(IEnumerable<Subscription> subscriptions)
+    {
+        if (subscriptions is null || !subscriptions.Any())
+            return 0;
+
+        var price = subscriptions.Sum(x => x.Cost);
+        return price;
+    }
+
+    public decimal GetYearlyCost(Subscription subscription)
     {
         if (subscription is null)
             throw new ArgumentNullException(nameof(subscription));
 
         return subscription.Cost * _monthsInAYear;
+    }
+
+    public IEnumerable<Subscription> GetSubscriptionListByMonth(IEnumerable<Subscription> subscriptions, int month)
+    {
+        var subscriptionsByMonth = subscriptions
+                    .Where(s => s.LastPayment.Month != month)
+                    .ToList();
+
+        return subscriptionsByMonth;
     }
 }
