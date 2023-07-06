291d411cee953a7cb24092b23b2e45dd12f22e94
Pituzek <piotr.rac@gmail.com>
2023-07-06 23:33:39 +0200

    Code cleanup

--- subtrack.MAUI/DateAndTimeProvider/DateTimeProvider.cs
+++ subtrack.MAUI/DateAndTimeProvider/DateTimeProvider.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace subtrack.MAUI.DateAndTimeProvider
+﻿namespace subtrack.MAUI.DateAndTimeProvider
 {
     public class DateTimeProvider : IDateTimeProvider
     {
