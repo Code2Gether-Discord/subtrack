022ae70ecab469135030c60730a042ebc8db1472
Pituzek <piotr.rac@gmail.com>
2023-07-06 23:19:51 +0200

    Added DateTimeProvider which can be injected into classes (because of that we can mock this in tests, and provide accurate time).

---
+++ subtrack.MAUI/DateAndTimeProvider/DateTimeProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace subtrack.MAUI.DateAndTimeProvider
+{
+    public class DateTimeProvider : IDateTimeProvider
+    {
+        public DateTimeOffset Now => DateTimeOffset.Now;
+    }
+}
---
+++ subtrack.MAUI/DateAndTimeProvider/IDateTimeProvider.cs
@@ -0,0 +1,7 @@
+﻿namespace subtrack.MAUI.DateAndTimeProvider
+{
+    public interface IDateTimeProvider
+    {
+        public DateTimeOffset Now { get; }
+    }
+}
