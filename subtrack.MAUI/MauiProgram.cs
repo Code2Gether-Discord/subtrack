5ed4e752e39a9fa578c2557a2df0eed1fda2de4d
Pituzek <piotr.rac@gmail.com>
2023-07-07 23:32:46 +0200

    Added services for DateTimeProvider

--- subtrack.MAUI/MauiProgram.cs
+++ subtrack.MAUI/MauiProgram.cs
@@ -26,6 +26,7 @@ public static class MauiProgram
         builder.Services.AddDbContext<SubtrackDbContext>(opt => opt.UseSqlite(dbConnectionString));
 
         builder.Services.AddScoped<ISubscriptionService, SubscriptionService>();
+        builder.Services.AddScoped<IDateTimeProvider, DateTimeProvider>();
         builder.Services.AddScoped<ISubscriptionsCalculator, SubscriptionsCalculator>();
 
         using var sp = builder.Services.BuildServiceProvider();
