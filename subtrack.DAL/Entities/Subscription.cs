b77d1e4b60acb6be32fe5b293d12a7949c836eab
grodis <grodiscostas@hotmail.com>
2023-07-16 15:40:42 +0300

    #58 Added maximum length for service titles.

--- subtrack.DAL/Entities/Subscription.cs
+++ subtrack.DAL/Entities/Subscription.cs
@@ -7,6 +7,7 @@ public class Subscription
     public int Id { get; set; }
 
     [Required]
+    [StringLength(20,ErrorMessage = "Service name should be less than 20 characters")]
     public string Name { get; set; } = null!;
     public string? Description { get; set; }
     public bool IsAutoPaid { get; set; }
