b46fe10ee48d982b8f14cff6b305152785dd6056
Konstantinos Grontis <grodiscostas@hotmail.com>
2023-07-16 17:06:27 +0300

    Add maximum title length validation for subscriptions (#61)
    * #58 Added maximum length for service titles.
    
    * #58 Added a missing dot to the error message.
    
    * #58 Changed max limit to 12 chars

--- subtrack.DAL/Entities/Subscription.cs
+++ subtrack.DAL/Entities/Subscription.cs
@@ -7,6 +7,7 @@ public class Subscription
     public int Id { get; set; }
 
     [Required]
+    [StringLength(12,ErrorMessage = "Service name should be less than 12 characters.")]
     public string Name { get; set; } = null!;
     public string? Description { get; set; }
     public bool IsAutoPaid { get; set; }
