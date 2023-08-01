49b2c9d55842b27ac6b264ea31bea89c50a8e597
Konstantinos Grontis <grodiscostas@hotmail.com>
2023-08-01 16:38:34 +0300

    #73 Added autopaid filtering (#77)
    * #73 Added autopaid filtering
    
    * #73 Changes related to  review comments of pr #77

--- subtrack.MAUI/Services/Abstractions/ISubscriptionService.cs
+++ subtrack.MAUI/Services/Abstractions/ISubscriptionService.cs
@@ -1,10 +1,11 @@
 ﻿using subtrack.DAL.Entities;
+using subtrack.MAUI.Utilities;
 
 namespace subtrack.MAUI.Services.Abstractions
 {
     public interface ISubscriptionService
     {
-        Task<IEnumerable<Subscription>> GetSubscriptions();
+        Task<IEnumerable<Subscription>> GetSubscriptions(GetSubscriptionsFilter? filter = null);
         Task<Subscription> CreateSubscriptionAsync(Subscription subscriptionToCreate);
         Task<Subscription?> GetById(int id);
         Task Delete(int id);
---
+++ subtrack.MAUI/Utilities/QueryableExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq.Expressions;
+
+namespace subtrack.MAUI.Utilities;
+
+public static class QueryableExtensions
+{
+    public static IQueryable<TSource> WhereIf<TSource>(this IQueryable<TSource> source, Expression<Func<TSource,bool>>? filter, bool shouldFilter)
+    {
+        if(filter is not null && shouldFilter) source.Where(filter);
+        return source;
+    }
+}
