d1d83fa323f8b56f672517dbdd5e9bb2b42ddaa9
Ashwin Thyagarajan <ashwin031192@gmail.com>
2023-08-16 20:25:32 +0530

    Allow for longer subscription titles and handle title overflow in subscription card

--- subtrack.DAL/Entities/Subscription.cs
+++ subtrack.DAL/Entities/Subscription.cs
@@ -7,7 +7,7 @@ public class Subscription
     public int Id { get; set; }
 
     [Required]
-    [StringLength(12,ErrorMessage = "Service name should be less than 12 characters.")]
+    [StringLength(20,ErrorMessage = "Service name should be less than 20 characters.")]
     public string Name { get; set; } = null!;
     public string? Description { get; set; }
     public bool IsAutoPaid { get; set; }
