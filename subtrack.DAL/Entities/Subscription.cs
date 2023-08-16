0eb4b76c6174638bbaf25897e2e302801f749795
Ashwin Thyagarajan <ashwin031192@gmail.com>
2023-08-16 21:17:09 +0530

    Update Subscription.cs

--- subtrack.DAL/Entities/Subscription.cs
+++ subtrack.DAL/Entities/Subscription.cs
@@ -7,7 +7,7 @@ public class Subscription
     public int Id { get; set; }
 
     [Required]
-    [StringLength(20,ErrorMessage = "Service name should be less than 20 characters.")]
+    [StringLength(50,ErrorMessage = "Service name should be less than 50 characters.")]
     public string Name { get; set; } = null!;
     public string? Description { get; set; }
     public bool IsAutoPaid { get; set; }
