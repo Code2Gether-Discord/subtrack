187434d7d2f7764bce4737488e24ba2a1965f969
Ashwin Thyagarajan <ashwin031192@gmail.com>
2023-08-19 12:16:25 +0530

    Uses Billing Cycles while fetching subscriptions for a month

--- subtrack.Tests/SubscriptionCalculatorTests/GetSubscriptionListByMonthTests.cs
+++ subtrack.Tests/SubscriptionCalculatorTests/GetSubscriptionListByMonthTests.cs
@@ -2,6 +2,7 @@
 using subtrack.MAUI.Services;
 using NSubstitute;
 using subtrack.MAUI.Services.Abstractions;
+using subtrack.Tests.Utilities;
 
 namespace subtrack.Tests.SubscriptionCalculatorTests
 {
@@ -9,11 +10,13 @@ namespace subtrack.Tests.SubscriptionCalculatorTests
     {
         private readonly ISubscriptionsCalculator _sut;
         private readonly IDateProvider _dateTimeProvider = Substitute.For<IDateProvider>();
-
+        private IEqualityComparer<Subscription> _subscriptionComparer;
+        private readonly DateTime today = new(2023, 4, 18);
         public GetSubscriptionListByMonthTests()
         {
             _sut = new SubscriptionsCalculator(_dateTimeProvider);
-            _dateTimeProvider.Today.Returns(DateTime.Now);
+            _dateTimeProvider.Today.Returns(today);
+            _subscriptionComparer = new SubscriptionEqualityComparer();
         }
 
         [Fact]
@@ -23,24 +26,24 @@ namespace subtrack.Tests.SubscriptionCalculatorTests
             IList<Subscription> subscriptions = CreateSubscriptions();
 
             // Act
-            var result = _sut.GetSubscriptionListByMonth(subscriptions, DateTime.Now);
+            var result = _sut.GetSubscriptionListByMonth(subscriptions, today).ToList();
 
             // Assert
-            Assert.Contains(subscriptions[0], result);
-            Assert.Contains(subscriptions[1], result);
+            Assert.Contains(subscriptions[0], result, _subscriptionComparer);
+            Assert.Contains(subscriptions[1], result, _subscriptionComparer);
         }
 
         [Fact]
         public void GetSubscriptionListByMonth_DoesNotReturn_SubscriptionsWithNonStartedPayments()
         {
             // Arrange
-            IList<Subscription> subscriptions = new[] { new Subscription() { LastPayment = DateTime.Now.AddMonths(1) } };
+            IList<Subscription> subscriptions = new[] { new Subscription() { LastPayment = today.AddMonths(1) } };
 
             // Act
-            var result = _sut.GetSubscriptionListByMonth(subscriptions, DateTime.Now);
+            var result = _sut.GetSubscriptionListByMonth(subscriptions, today).ToList();
 
             // Assert
-            Assert.DoesNotContain(subscriptions[0], result);
+            Assert.DoesNotContain(subscriptions[0], result, _subscriptionComparer);
         }
 
         [Fact]
@@ -50,17 +53,46 @@ namespace subtrack.Tests.SubscriptionCalculatorTests
             IList<Subscription> subscriptions = CreateSubscriptions();
 
             // Act
-            var result = _sut.GetSubscriptionListByMonth(subscriptions, DateTime.Now);
+            var result = _sut.GetSubscriptionListByMonth(subscriptions, today).ToList();
 
             // Assert
-            Assert.DoesNotContain(subscriptions[2], result);
-            Assert.DoesNotContain(subscriptions[3], result);
+            Assert.DoesNotContain(subscriptions[2], result, _subscriptionComparer);
+            Assert.DoesNotContain(subscriptions[3], result, _subscriptionComparer);
         }
 
-        public static IList<Subscription> CreateSubscriptions()
+        [Fact]
+        public void GetSubscriptionListByMonth_MonthProvided_ShouldReturnWeeklySubscriptionsMultipleTimes()
         {
-            DateTime today = DateTime.Today;
+            // Arrange
+            IList<Subscription> subscriptions = CreateSubscriptions();
 
+            // Act
+            var result = _sut.GetSubscriptionListByMonth(subscriptions, today.AddMonths(2)).ToList();
+
+            // Assert
+            Assert.True(result.Count(s => s.Name == subscriptions[5].Name) == 4);
+            Assert.True(result.Count(s => s.Name == subscriptions[4].Name) == 2);
+        }
+
+        [Fact]
+        public void GetSubscriptionListByMonth_MonthProvided_SubscriptionsHavingLongerThanMonthlyCycle_ShouldReturnAsExpected()
+        {
+            // Arrange
+            IList<Subscription> subscriptions = CreateSubscriptions();
+
+            // Act
+            List<Subscription> first = _sut.GetSubscriptionListByMonth(subscriptions, today.AddMonths(2)).ToList(),
+                second = _sut.GetSubscriptionListByMonth(subscriptions, today.AddMonths(4)).ToList(),
+                third = _sut.GetSubscriptionListByMonth(subscriptions, today.AddMonths(5)).ToList();
+
+            // Assert
+            Assert.Contains(subscriptions[6], first, _subscriptionComparer);
+            Assert.DoesNotContain(subscriptions[6], second, _subscriptionComparer);
+            Assert.Contains(third, s => s.Name == subscriptions[6].Name);
+        }
+
+        private IList<Subscription> CreateSubscriptions()
+        {
             var firstSub = new Subscription
             {
                 Id = 1,
@@ -68,7 +100,10 @@ namespace subtrack.Tests.SubscriptionCalculatorTests
                 IsAutoPaid = true,
                 LastPayment = today.AddMonths(-1),
                 Name = "Netflix",
-                Description = ""
+                Description = "",
+                FirstPaymentDay = 1,
+                BillingOccurrence = BillingOccurrence.Month,
+                BillingInterval = 1,
             };
 
             var secondSub = new Subscription
@@ -78,7 +113,10 @@ namespace subtrack.Tests.SubscriptionCalculatorTests
                 IsAutoPaid = false,
                 LastPayment = today.AddMonths(-1),
                 Name = "AmazonPrime",
-                Description = ""
+                Description = "",
+                FirstPaymentDay = 1,
+                BillingOccurrence = BillingOccurrence.Month,
+                BillingInterval = 1,
             };
 
             var thirdSub = new Subscription
@@ -88,7 +126,10 @@ namespace subtrack.Tests.SubscriptionCalculatorTests
                 IsAutoPaid = true,
                 LastPayment = today,
                 Name = "Disney+",
-                Description = ""
+                Description = "",
+                FirstPaymentDay = 1,
+                BillingOccurrence = BillingOccurrence.Month,
+                BillingInterval = 1,
             };
 
             var fourthSub = new Subscription
@@ -98,10 +139,52 @@ namespace subtrack.Tests.SubscriptionCalculatorTests
                 IsAutoPaid = false,
                 LastPayment = today,
                 Name = "HboMax",
-                Description = ""
+                Description = "",
+                FirstPaymentDay = 1,
+                BillingOccurrence = BillingOccurrence.Month,
+                BillingInterval = 1,
             };
 
-            return new List<Subscription> { firstSub, secondSub, thirdSub, fourthSub};
+            var fifthSub = new Subscription
+            {
+                Id = 4,
+                Cost = 19,
+                IsAutoPaid = false,
+                LastPayment = today.AddMonths(1).AddDays(2),
+                Name = "Curiosity Stream",
+                Description = "",
+                FirstPaymentDay = 1,
+                BillingOccurrence = BillingOccurrence.Week,
+                BillingInterval = 2,
+            };
+
+            var sixthSub = new Subscription
+            {
+                Id = 4,
+                Cost = 19,
+                IsAutoPaid = false,
+                LastPayment = today.AddMonths(1).AddDays(2),
+                Name = "Nebula",
+                Description = "",
+                FirstPaymentDay = 1,
+                BillingOccurrence = BillingOccurrence.Week,
+                BillingInterval = 1,
+            };
+
+            var seventhSub = new Subscription
+            {
+                Id = 4,
+                Cost = 19,
+                IsAutoPaid = false,
+                LastPayment = today.AddMonths(-1).AddDays(2),
+                Name = "Youtube Premium",
+                Description = "",
+                FirstPaymentDay = 1,
+                BillingOccurrence = BillingOccurrence.Month,
+                BillingInterval = 3,
+            };
+
+            return new List<Subscription> { firstSub, secondSub, thirdSub, fourthSub, fifthSub, sixthSub, seventhSub };
         }
     }
 }
---
+++ subtrack.Tests/Utilities/SubscriptionEqualityComparer.cs
@@ -0,0 +1,35 @@
+﻿using subtrack.DAL.Entities;
+using System.Diagnostics.CodeAnalysis;
+
+namespace subtrack.Tests.Utilities
+{
+    internal class SubscriptionEqualityComparer : IEqualityComparer<Subscription>
+    {
+        public bool Equals(Subscription? x, Subscription? y)
+        {
+            if (x is null && y is null) return true;
+            if (x is null || y is null) return false;
+            return x.Id == y.Id && 
+                   x.Name == y.Name && 
+                   x.Description == y.Description && 
+                   x.IsAutoPaid == y.IsAutoPaid && 
+                   x.Cost == y.Cost &&
+                   x.FirstPaymentDay == y.FirstPaymentDay &&
+                   x.LastPayment == y.LastPayment &&
+                   x.BillingOccurrence == y.BillingOccurrence &&
+                   x.BillingInterval == y.BillingInterval;
+        }
+
+        public int GetHashCode([DisallowNull] Subscription obj)
+        {
+            return obj.Id.GetHashCode() + 
+                   obj.Name.GetHashCode() + 
+                   (obj.Description?.GetHashCode() ?? 0) + obj.IsAutoPaid.GetHashCode() + 
+                   obj.Cost.GetHashCode() +
+                   obj.FirstPaymentDay.GetHashCode() + 
+                   obj.LastPayment.GetHashCode() + 
+                   obj.BillingOccurrence.GetHashCode() +
+                   obj.BillingInterval.GetHashCode();
+        }
+    }
+}
