bd47ba17bca103e85784da9e7cc09f13eb2b51f7
grodis <grodiscostas@hotmail.com>
2023-09-09 19:54:01 +0300

    #114 Implemented CssUtil.GetDueClass & tests

---
+++ subtrack.MAUI/Utilities/CssUtil.cs
@@ -0,0 +1,13 @@
+﻿namespace subtrack.MAUI.Utilities;
+
+public static class CssUtil
+{
+    public static string GetDueClass(int daysUntilDue) =>
+        daysUntilDue switch
+        {
+            <= 0 => ".overdue",
+            (> 0) and (<= 2) => ".text-warning",
+            _ => string.Empty,
+        };
+
+}
---
+++ subtrack.Tests/UtilitiesTests/GetDueClassTests.cs
@@ -0,0 +1,24 @@
+﻿using subtrack.MAUI.Utilities;
+
+namespace subtrack.Tests.UtilitiesTests;
+
+public class GetDueClassTests
+{
+    [Theory]
+    [InlineData(-1, ".overdue")]
+    [InlineData(0, ".overdue")]
+    [InlineData(1, ".text-warning")]
+    [InlineData(2, ".text-warning")]
+    [InlineData(3, "")]
+    public void GetDueClass_ReturnsExpectedResult(int daysUntilDue, string expected)
+    {
+        // Arrange
+        // No need to arrange anything as this is a pure function.
+
+        // Act
+        string result = CssUtil.GetDueClass(daysUntilDue);
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+}
