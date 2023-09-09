082ff1bb21e9aab1153fc726c32bf375ccfa269b
Konstantinos Grontis <grodiscostas@hotmail.com>
2023-09-09 22:57:02 +0300

    #114 Implemented CssUtil.GetDueClass & tests (#123)
    * #114 Implemented CssUtil.GetDueClass & tests
    
    * #114 Added semicolor to css rule
    
    * #114 Changed placement of css rule.
    
    * #114 removed dots from string result.
    
    * #114 PR review changes.

---
+++ subtrack.Tests/Utilities/CssUtilTests.cs
@@ -0,0 +1,18 @@
+﻿using subtrack.MAUI.Utilities;
+
+namespace subtrack.Tests.Utilities;
+
+public class CssUtilTests
+{
+    [Theory]
+    [InlineData(-1, "overdue")]
+    [InlineData(0, "text-warning")]
+    [InlineData(1, "text-warning")]
+    [InlineData(2, "text-warning")]
+    public void GetDueClass_ReturnsExpectedResult(int daysUntilDue, string expected)
+    {
+        string result = CssUtil.GetDueClass(daysUntilDue);
+
+        Assert.Equal(expected, result);
+    }
+}
