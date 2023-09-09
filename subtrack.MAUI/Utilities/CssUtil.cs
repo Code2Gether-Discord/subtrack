de1c6749dac4364d0f61543bf30493258126254f
grodis <grodiscostas@hotmail.com>
2023-09-09 21:22:00 +0300

    #114 PR review changes.

--- subtrack.MAUI/Utilities/CssUtil.cs
+++ subtrack.MAUI/Utilities/CssUtil.cs
@@ -5,8 +5,8 @@ public static class CssUtil
     public static string GetDueClass(int daysUntilDue) =>
         daysUntilDue switch
         {
-            <= 0 => "overdue",
-            (> 0) and (<= 2) => "text-warning",
+            < 0 => "overdue",
+            (>= 0) and (<= 2) => "text-warning",
             _ => string.Empty,
         };
 
--- subtrack.Tests/Utilities/CssUtilTests.cs
+++ subtrack.Tests/Utilities/CssUtilTests.cs
@@ -1,24 +1,18 @@
 ﻿using subtrack.MAUI.Utilities;
 
-namespace subtrack.Tests.UtilitiesTests;
+namespace subtrack.Tests.Utilities;
 
-public class GetDueClassTests
+public class CssUtilTests
 {
     [Theory]
     [InlineData(-1, "overdue")]
-    [InlineData(0, "overdue")]
+    [InlineData(0, "text-warning")]
     [InlineData(1, "text-warning")]
     [InlineData(2, "text-warning")]
-    [InlineData(3, "")]
     public void GetDueClass_ReturnsExpectedResult(int daysUntilDue, string expected)
     {
-        // Arrange
-        // No need to arrange anything as this is a pure function.
-
-        // Act
         string result = CssUtil.GetDueClass(daysUntilDue);
 
-        // Assert
         Assert.Equal(expected, result);
     }
 }
