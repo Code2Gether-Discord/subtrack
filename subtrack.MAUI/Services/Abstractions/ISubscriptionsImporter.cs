122f997696a2414426a8a4db609718f64f81260f
ChrisK00 <christian.khalil2000@gmail.com>
2023-12-07 21:53:38 +0100

    import

---
+++ subtrack.MAUI/Services/Abstractions/ISubscriptionsImporter.cs
@@ -0,0 +1,8 @@
+﻿using subtrack.DAL.Entities;
+
+namespace subtrack.MAUI.Services.Abstractions;
+
+public interface ISubscriptionsImporter
+{
+    Task<IReadOnlyCollection<Subscription>> ImportFromCsvAsync(Stream content);
+}
---
+++ subtrack.MAUI/Utilities/CsvMappings.cs
@@ -0,0 +1,14 @@
+﻿using CsvHelper.Configuration;
+using subtrack.DAL.Entities;
+using System.Globalization;
+
+namespace subtrack.MAUI.Utilities;
+
+public class SubscriptionCsvMapping : ClassMap<Subscription>
+{
+    public SubscriptionCsvMapping()
+    {
+        AutoMap(CultureInfo.InvariantCulture);
+        Map(m => m.Id).Ignore();
+    }
+}
