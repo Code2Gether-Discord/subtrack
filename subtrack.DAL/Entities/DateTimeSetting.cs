c671e83f717a957e87f98432bc32ccb28644b599
chrisK00 <72318023+chrisK00@users.noreply.github.com>
2023-12-11 22:08:44 +0100

    added lastsubexporttime (#162)

--- subtrack.DAL/Entities/DateTimeSetting.cs
+++ subtrack.DAL/Entities/DateTimeSetting.cs
@@ -3,6 +3,7 @@
     public class DateTimeSetting : SettingsBase
     {
         public const string LastAutoPaymentTimeStampKey = "LastAutoPaymentTimeStamp";
+        public const string LastSubscriptionExportTimeStampKey = "LastSubscriptionExportTimeStamp";
         public DateTime? Value { get; set; }
     }
 }
